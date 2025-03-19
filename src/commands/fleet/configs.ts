0bfcbc80c6e7339427237179bd6dd49b0bf8106d
Julian Imhof <julian.imhof@4players.io>
2025-03-19 13:12:58 +0100

    fix: remove outdated default values

--- src/commands/fleet/configs.ts
+++ src/commands/fleet/configs.ts
@@ -8,7 +8,6 @@ import {
   Mount,
   PortDefinition,
   Protocol,
-  PublishMode,
   RestartPolicy,
   RestartPolicyCondition,
   ServerConfig,
@@ -246,7 +245,6 @@ const createConfig = new Command()
         ports.push({
           name: portDefinition.name!,
           targetPort: portDefinition.targetPort!,
-          publishMode: PublishMode.Ingress, // We default to Ingress for now
           protocols: [portDefinition.protocol] as Protocol[],
         });
       }
